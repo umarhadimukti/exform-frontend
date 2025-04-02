b5c475588f73b9bc1622bae49eca2420627c8339
Umar Hadi Mukti <umarhadimukti@gmail.com>
2025-04-02 22:03:36 +0700

    fix svg

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -122,8 +122,8 @@ const Login: React.FC = () => {
                             <div className="flex flex-col gap-2 sm:gap-0 sm:flex-row sm:justify-between sm:items-center">
                                 <Button type="submit" className='cursor-pointer' disabled={isLoading}>
                                     { isLoading ? 
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width={4} className='animate-spin'>
-                                            <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" width={4} className='animate-spin'>
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
                                         </svg> : 'Login'
                                     }
                                 </Button>
