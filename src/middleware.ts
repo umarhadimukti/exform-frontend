9afa8e9d6f876cf05a4a16998c672b7dcb31e92a
Umar Hadi Mukti <umarhadimukti@gmail.com>
2025-04-05 16:41:37 +0700

    config middleware

---
+++ src/middleware.ts
@@ -0,0 +1,25 @@
+import { Router } from "lucide-react";
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware (request: NextRequest)
+{
+    const { cookies } = request;
+    const accessToken = cookies.get('accessToken');
+
+    // if there's no token available and trying to access protected page
+    if (!accessToken && request.nextUrl.pathname.startsWith('/login')) {
+        return NextResponse.redirect(new URL('/login', request.url));
+    }
+
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: [
+        '/forms/:path*',
+        '/users/:path*',
+        '/questions/:path*',
+        '/answers/:path*',
+        '/response/:path*',
+    ],
+}
