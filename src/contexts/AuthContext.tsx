e7d8038c167bb9d94624944666fafdccda650765
Umar Hadi Mukti <umarhadimukti@gmail.com>
2025-04-05 11:23:44 +0700

    fix auth context

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,75 @@
+import { createContext, useContext, useState, useEffect } from "react";
+import { User, AuthContextType } from "@/types/authContextTypes";
+import { useRouter } from "next/navigation";
+
+// create AuthContext
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+// create AuthProvider
+export const AuthProvider = ({ children }: { children: React.ReactNode }) =>
+{
+    // define state
+    const [ user, setUser ] = useState<User | null>(null);
+    const [ loading, setLoading ] = useState<boolean>(false);
+    const router = useRouter();
+
+    useEffect(() => {
+
+        const checkUserLoggedIn = async () => {
+            try {
+                const response = await fetch('http://localhost:3002/api/v1/current-user');
+
+                if (response.ok) {
+                    const data = await response.json();
+                    setUser(data);
+                }
+    
+                setLoading(false);
+            } catch (error) {
+                setLoading(false);
+                throw new Error(`authentication check failed: ${error instanceof Error ? error.message : error}`);
+            }
+        }
+
+        checkUserLoggedIn();
+    }, [])
+
+    const logout = async (): Promise<void> => {
+        try {
+            const response = await fetch('/api/logout', {
+                method: 'POST',
+                credentials: 'include',
+                headers: {
+                'Content-Type': 'application/json'
+                }
+            });
+    
+            const data = await response.json();
+        
+            if (data.status) {
+                // clear user state
+                setUser(null);
+                // redirect to login page
+                router.push('/login');
+            } else {
+                throw new Error(data.message);
+            }
+        } catch (error) {
+            console.error('logout failed:', error instanceof Error ? error.message : String(error));
+        }
+    };
+
+    return (
+        <AuthContext.Provider value={{ user, setUser, logout, loading }}>
+          {children}
+        </AuthContext.Provider>
+    );
+}
+
+export const useAuth = (): AuthContextType => {
+    const context = useContext(AuthContext);
+    if (context === undefined) {
+        throw new Error('useAuth must be used within an AuthProvider');
+    }
+    return context;
+};
