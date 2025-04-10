24ad93f4688eb41bc7ee58c0edff0216a7ec45bd
Umar Hadi Mukti <umarhadimukti@gmail.com>
2025-04-10 20:29:34 +0700

    draft ui title n description form post

--- src/app/forms/c/page.tsx
+++ src/app/forms/c/page.tsx
@@ -35,7 +35,19 @@ const FormCreate: React.FC = () => {
         </div>
 
         <div className="bg-gray-100 w-full border min-h-screen">
-
+            <div className="w-5/10 mx-auto bg-white rounded mt-5">
+                <div className="bg-gray-500 rounded-t p-1"></div>
+                <div className="p-5 pb-7 flex flex-col gap-3">
+                    <input
+                        type="text"
+                        className='text-4xl text-gray-800 border-b-1 focus:border-b-2 p-1 border-gray-400 focus:border-gray-800 w-full outline-none ring-none transition-all'
+                        defaultValue='untitled form' />
+                    <input
+                        type="text"
+                        className='text-base text-gray-600 border-b-1 focus:border-b-2 p-1 border-gray-400 focus:border-gray-600 w-full outline-none ring-none transition-all'
+                        defaultValue='form description' />
+                </div>
+            </div>
         </div>
     </div>
   )
