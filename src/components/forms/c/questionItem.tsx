b73120c9ab220000e5f5bce99321b11efd6bdfc5
Umar Hadi Mukti <umarhadimukti@gmail.com>
2025-04-11 20:36:10 +0700

    fix style toggle

--- src/components/forms/c/questionItem.tsx
+++ src/components/forms/c/questionItem.tsx
@@ -182,16 +182,16 @@ const QuestionItem: React.FC<QuestionItemProps> = ({ question, index, isLast, on
                         ) }
                     </div>
                 </div>
-                <div className="mt-2">
-                    <div className='flex justify-end gap-3'>
-                        <div className='group'>
+                <div className="mt-1">
+                    <div className='flex justify-end gap-3 items-center'>
+                        <div className='group p-3 rounded-full hover:bg-gray-100 transition-all duration-300'>
                             <PiTrashLight className='group-hover:text-red-500 transition-all duration-300 cursor-pointer text-xl'/>
                         </div>
                         <div>
                             <label className="inline-flex items-center cursor-pointer">
                             <input type="checkbox" value="" className="sr-only peer" />
-                            <div className="relative w-9 h-5 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-blue-600"></div>
-                            <span className="ms-3 text-sm font-medium text-gray-900">required</span>
+                            <div className="relative w-9 h-3 bg-indigo-200 peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border after:content-[''] after:absolute after:top-[-4px] after:start-[-1px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-indigo-600"></div>
+                            <span className="ml-2 text-sm font-light text-gray-700">required</span>
                             </label>
                         </div>
                     </div>
