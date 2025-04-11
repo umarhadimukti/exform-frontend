acd6bd95339e39290a81331fa41a868c823841d8
Umar Hadi Mukti <umarhadimukti@gmail.com>
2025-04-11 09:09:13 +0700

    add button question

--- src/components/forms/c/questionItem.tsx
+++ src/components/forms/c/questionItem.tsx
@@ -3,17 +3,25 @@ import { QuestionItem as QuestionItemProps } from '@/types/formQuestionTypes'
 
 const QuestionItem: React.FC<QuestionItemProps> = ({ question, index, isLast, onAdd, onDelete, onChange }) => {
   return (
-    <div>
-        <div className="p-5 pb-7 flex flex-col gap-3">
-            <input
-                type="text"
-                className='text-4xl text-gray-800 border-b-1 focus:border-b-2 p-1 border-gray-400 focus:border-gray-800 w-full outline-none ring-none transition-all'
-                defaultValue='untitled form' />
-            <input
-                type="text"
-                className='text-base text-gray-600 border-b-1 focus:border-b-2 p-1 border-gray-400 focus:border-gray-600 w-full outline-none ring-none transition-all'
-                defaultValue='form description' />
-        </div>
+    <div className='relative'>
+        {question.type === 'first' && (
+            <div className="p-5 pb-7 flex flex-col gap-3">
+                <input
+                    type="text"
+                    className='text-4xl text-gray-800 border-b-1 focus:border-b-2 p-1 border-gray-400 focus:border-gray-800 w-full outline-none ring-none transition-all'
+                    defaultValue={question.value ?? 'default title form'} />
+                <input
+                    type="text"
+                    className='text-base text-gray-600 border-b-1 focus:border-b-2 p-1 border-gray-400 focus:border-gray-600 w-full outline-none ring-none transition-all'
+                    defaultValue='form description' />
+            </div>
+        )}
+
+        {isLast && (
+            <div className='absolute -right-10 bottom-0 text-sm text-gray-800 hover:text-gray-900 bg-white hover:bg-gray-50 cursor-pointer rounded px-3 py-1'>
+                +
+            </div>
+        )}
     </div>
   )
 }
